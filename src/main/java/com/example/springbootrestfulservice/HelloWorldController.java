89b1cc81d170234f31aa1ef60b1a2efd3885682b
imwisdom <kjihye0340@naver.com>
2021-06-04 22:58:15 +0900

    Path Variable 사용

--- src/main/java/com/example/springbootrestfulservice/HelloWorldController.java
+++ src/main/java/com/example/springbootrestfulservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.springbootrestfulservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +19,12 @@ public class HelloWorldController {
         return new HelloWorldBean("Hello World");
         //RestController를 사용하면
         //ResponseBody를 하지 않아도 json 형식으로 저장되는 것을 볼 수 있음
+    }
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        //PathVariable 지정 : 가변값을 매개변수로 받음
+        //{name}의 name과 String name(매개변수 이름)은 같은 이름을 가져야됨
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 
