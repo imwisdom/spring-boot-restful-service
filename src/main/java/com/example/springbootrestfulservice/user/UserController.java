0c4901f14b715300f13db20732227624e17af4f8
imwisdom <kjihye0340@naver.com>
2021-06-06 18:02:48 +0900

    HTTP Status Code 제어

--- src/main/java/com/example/springbootrestfulservice/user/UserController.java
+++ src/main/java/com/example/springbootrestfulservice/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.springbootrestfulservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,13 +20,20 @@ public class UserController {
     }
     // ex) GET /users/1
     //path variable의 type을 지정한 것에 따라 id가 자동적으로 type에 맞게 매핑
-    @GetMapping("/user/{id}")
+    @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
     }
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = service.save(user);
+        //사용자에게 요청값을 변환해주기 위한 코드
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+        //201 Created
     }
 
 }
