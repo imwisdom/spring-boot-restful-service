30c7db9214fa437173641d3225622919bcb504ac
imwisdom <kjihye0340@naver.com>
2021-06-06 18:26:54 +0900

    Spring의 AOP를 이용한 Exception Handling

---
+++ src/main/java/com/example/springbootrestfulservice/exception/CustomizeResponseEnityExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.springbootrestfulservice.exception;
+
+import com.example.springbootrestfulservice.user.UserNotFoundException;
+import jdk.jfr.Enabled;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice   //모든 Controller가 실행이 될 때 얘가 사전에 실행되도록 함
+public class CustomizeResponseEnityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class) //이 메소드가 Exception handler로 수행할 수 있다는 것을 지정
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(UserNotFoundException.class) //UserNotFoundException이 발생되면 이 메소드가 발생됨
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+}
